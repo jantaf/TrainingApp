6fd7f83ed4db30d1f1b89daba8a9af3af471aa65
Jorge Anta <jorgeanta2@gmail.com>
2017-04-08 11:57:00 +0200

    Initial commit

---
+++ app/src/main/java/me/janta/trainingapp/Item.kt
@@ -0,0 +1,9 @@
+package me.janta.trainingapp
+
+
+fun getItems():List<Item>{
+    return (1..10).map { Item(it.toLong(),"Title $it","http:www.lorempixel.com/400/400/cats/$it/") }
+}
+data class Item(val id:Long,val title:String, val url:String) {
+
+}
